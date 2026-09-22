c3cbf91a03b1a8f6ab286a44d9ced0cc83edc8e4
dvihaan <dvihaan1@gmail.com>
2026-09-22 15:40:44 -0400

    Lesson 8

--- Lesson 8/CityLatitudes.m
+++ Lesson 8/CityLatitudes.m
@@ -1,6 +1,4 @@
-
-
-clear 
+clear
 close all
 
 %% Obtain cities data
@@ -17,15 +15,19 @@ lon = num(:,3);
 
 %% Calculate some basic stastics of the latitude data
 
-[maxLat, iMaxLat] =                  % complete to find the max latitude
-[minLat, iMinLat] =                  % complete to find the min latitude
-meanLat =                            % complete to find the mean latitude 
-medianLat =                          % complete to find the median latitude
-[modeLat, freqModeLat] =              % complete to find the mode of the latitudes
+[maxLat, iMaxLat] = max(lat);        % find the max latitude
+[minLat, iMinLat] = min(lat);        % find the min latitude
+meanLat = mean(lat);                 % find the mean latitude
+medianLat = median(lat);             % find the median latitude
+modeLat = mode(lat);                 % find the mode of the latitudes
+freqModeLat = sum(lat == modeLat);   % find how many times the mode occurs
+varLat = var(lat);                   % find the variance of the latitudes
+stdLat = std(lat);                   % find the standard deviation of the latitudes
 
 disp(['The most northern city is ' names{iMaxLat} ' (' num2str(maxLat) ' deg)']);
 disp(['The most southern city is ' names{iMinLat} ' (' num2str(minLat) ' deg)']);
 disp(['The mean latitude is ' num2str(meanLat) ' deg']);
 disp(['The median latitude is ' num2str(medianLat) ' deg']);
 disp(['The mode latitude is ' num2str(modeLat) ' deg']);
-
+disp(['The variance of the latitude is ' num2str(varLat) ' deg^2']);
+disp(['The standard deviation of the latitude is ' num2str(stdLat) ' deg']);
