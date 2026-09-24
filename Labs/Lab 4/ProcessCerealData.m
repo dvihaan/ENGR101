365d7e037c1ec8e53288cd6622a7e16d33a805db
dvihaan <dvihaan1@gmail.com>
2026-09-24 11:39:22 -0400

    processCerealData

---
+++ Labs/Lab 4/ProcessCerealData.m
@@ -0,0 +1,29 @@
+clear;
+close all;
+
+cereal = readmatrix('cereals.csv');
+
+figure();
+maxSugar = max(cereal(:,8));
+bins = -0.5:1:1 + maxSugar;
+histogram(cereal(:,8), bins);
+xlabel('Sugar (g)');
+ylabel('# of Cereals');
+title('Sugar Contents in Cereals');
+ax = gca;
+ax.FontSize = 14;
+grid on;
+
+print('sugars_histogram.png', '-dpng');
+
+figure();
+boxplot(cereal(:,4), cereal(:,3));
+xlabel('Cereal Manufacturer (Code)');
+ylabel('Calories per Serving');
+title('Calories for Cereals by Manufacturer');
+xticklabels({"G", "K", "P", "Q", "R"});
+ax = gca;
+ax.FontSize = 14;
+grid on;
+
+print('cals_by_manufacturer_boxplots.png', '-dpng');
